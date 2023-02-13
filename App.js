86aed741e29bbaa4434b692f91a356410d3f4e77
dhsong215 <77286910+dhsong215@users.noreply.github.com>
2023-02-14 03:29:02 +0900

    assets and fonts preload

--- App.js
+++ App.js
@@ -1,12 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
+import { useAssets } from 'expo-asset';
+import * as SplashScreen from 'expo-splash-screen';
+import { Ionicons } from '@expo/vector-icons';
+
+import React, { useEffect, useState, useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  //useStates
+  const [appReady, setAppReady] = useState(false);
+
+  //assetsReady
+  // const [fontsLoaded] = useFonts({
+  //   BMEuljiro: require('./assets/fonts/BMEuljiro10yearslaterOTF.otf'),
+  //   Baemin: require('./assets/fonts/BMDOHYEON_otf.otf'),
+  // });
+
+  const fonts = [
+    Font.loadAsync('baemin', require('./assets/fonts/BMDOHYEON_otf.otf')),
+    Font.loadAsync(
+      'baeminEuljiro',
+      require('/Users/dhsong/Desktop/myMovie/assets/fonts/BMEuljiro10yearslaterOTF.otf')
+    ),
+  ];
+
+  const imgs = [Asset.loadAsync(require('./assets/img/cat.jpg'))];
+
+  //useEffect
+  useEffect(() => {
+    const prepare = async () => {
+      await Promise.all([...fonts, ...imgs]);
+      setAppReady(true);
+    };
+    prepare();
+  }, []);
+
+  //appReady -> Splashscreen off
+  const onLayoutChange = useCallback(async () => {
+    if (appReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appReady]);
+
+  if (!appReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutChange}>
+      <Text
+        style={{
+          fontFamily: 'baeminEuljiro',
+          fontSize: 60,
+        }}
+      >
+        배민-을지로체
+      </Text>
+      <Text style={{ fontFamily: 'baemin' }}>
+        배달의민족 폰트 가져오기 테스트
+      </Text>
     </View>
   );
 }
