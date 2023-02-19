2137763c9685b7cd0c776e4cec33e157039dcec9
dhsong215 <77286910+dhsongdev@users.noreply.github.com>
2023-02-20 00:13:29 +0900

    add sliders

---
+++ components/Slider.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+
+import styled from 'styled-components';
+
+const imgPath = (path) => {
+  return `https://image.tmdb.org/t/p/w500${path}`;
+};
+
+const Container = styled.View`
+  width: 100%;
+`;
+
+const Title = styled.Text`
+  font-size: 30px;
+  padding: 10px;
+  color: ${(props) => props.theme.mainTextColor};
+`;
+
+const PosterSlider = styled.ScrollView`
+  padding: 0px 10px 10px 10px;
+`;
+
+const PosterImg = styled.Image`
+  height: 240px;
+  width: 160px;
+  border-radius: 10px;
+`;
+
+const PosterTitle = styled.Text`
+  font-size: 15px;
+`;
+
+export default function Slider({ data, title }) {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <PosterSlider showsHorizontalScrollIndicator={false} horizontal={true}>
+        {data.map((data, index) => (
+          <View style={{ margin: 5 }} key={data.id}>
+            <TouchableOpacity>
+              <PosterImg
+                source={{
+                  uri: `${imgPath(data.poster_path)}`,
+                }}
+              />
+            </TouchableOpacity>
+            {/* <PosterTitle>{data.original_title}</PosterTitle> */}
+          </View>
+        ))}
+      </PosterSlider>
+    </Container>
+  );
+}
--- screens/Movie.js
+++ screens/Movie.js
@@ -1,9 +1,12 @@
 //react components
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styled from 'styled-components/native';
 
 import MovieTopBanner from '../components/Banner';
+import Slider from '../components/Slider';
+
+import { API_KEY } from '@env';
 
 //Main components: container
 const Main = styled.ScrollView`
@@ -12,9 +15,33 @@ const Main = styled.ScrollView`
 
 //screen
 export default function Movie() {
+  const [populars, setPopulars] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getpopulars = async () => {
+    const data = await (
+      await fetch(
+        'https://api.themoviedb.org/3/movie/popular?api_key=bbbb0a1d7e005e258af9072da3838e01&language=en-US'
+      )
+    ).json();
+    setPopulars(data.results);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getpopulars();
+  }, []);
+
   return (
     <Main>
       <MovieTopBanner />
+      {loading === true ? null : (
+        <Slider data={populars} title={'Popular Now'} />
+      )}
+      {loading === true ? null : (
+        <Slider data={populars} title={'Trending Now'} />
+      )}
+      {loading === true ? null : <Slider data={populars} title={'Random'} />}
     </Main>
   );
 }
